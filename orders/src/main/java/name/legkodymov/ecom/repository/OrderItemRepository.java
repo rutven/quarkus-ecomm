f33098cc23627bf7b7a0c5cd9e5ec73f4e7c56bb
Sergey <rutven@gmail.com>
2022-11-05 19:41:17 +0300

    Clean up - removed unused classes and modified hibrnate update rule

--- orders/src/main/java/name/legkodymov/ecom/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package name.legkodymov.ecom.repository;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import name.legkodymov.ecom.model.OrderItem;
-
-public class OrderItemRepository implements PanacheRepository<OrderItem> {
-}
