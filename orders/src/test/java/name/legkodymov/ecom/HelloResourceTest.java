bf45e7cf77989e8e2da7d4642a8fcb450dfa1b29
Sergey <rutven@gmail.com>
2023-02-11 22:05:09 +0300

    Added hello resource and fixed tests

---
+++ orders/src/test/java/name/legkodymov/ecom/HelloResourceTest.java
@@ -0,0 +1,29 @@
+package name.legkodymov.ecom;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class HelloResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello, world!"));
+    }
+
+    @Test
+    public void testHelloUserEndpoint() {
+        given()
+                .when().get("/hello/user/Ivan")
+                .then()
+                .statusCode(200)
+                .body(is("Hello, Ivan!"));
+    }
+}
